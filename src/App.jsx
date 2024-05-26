2c342ac31b3d3f139e8dc19e23f70ab8f8ecead1
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2024-05-26 17:21:24 -0500

    FEATURE:GithubPages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { RouterProvider } from 'react-router-dom';
-import  router  from './routes/routes';
+import router from './routes/routes';
 
 const id_client = import.meta.env.VITE_ID_CLIENT_GOOGLE;
 function App() {
-
 	return (
-		<GoogleOAuthProvider clientId={id_client} >
-      <RouterProvider router={router} />
+		<GoogleOAuthProvider clientId={id_client}>
+			<RouterProvider router={router} />
 		</GoogleOAuthProvider>
 	);
 }
