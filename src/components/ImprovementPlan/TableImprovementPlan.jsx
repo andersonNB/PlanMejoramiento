b0357a8fe33d9ed768aa48ccf774dd4748258b12
Anderson Navarro <andersonaugustonb@ufps.edu.co>
2024-06-15 14:18:29 -0500

    cambios locales formateo

--- src/components/ImprovementPlan/TableImprovementPlan.jsx
+++ src/components/ImprovementPlan/TableImprovementPlan.jsx
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from 'react';
-import { SearchOutlined } from '@ant-design/icons';
-import { Col, Row, Form, Input, Button, Select, Table, Space } from 'antd';
+import { DeleteOutlined, EditOutlined, SearchOutlined } from '@ant-design/icons';
+import { Input, Button, Table, Space, Typography, Popconfirm, Modal } from 'antd';
 import PropTypes from 'prop-types';
 /*import Highlighter from 'react-highlight-words';*/
 
-
+// eslint-disable-next-line react/display-name
 const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 	const [searchText, setSearchText] = useState('');
 	const [searchedColumn, setSearchedColumn] = useState('');
 	const searchInput = useRef(null);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
 	const handleSearch = (selectedKeys, confirm, dataIndex) => {
 		confirm();
 		setSearchText(selectedKeys[0]);
@@ -23,7 +25,7 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 		filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
 			<div
 				style={{
-					padding: 8
+					padding: 8,
 				}}
 				onKeyDown={(e) => e.stopPropagation()}
 			>
@@ -35,36 +37,36 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 					onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
 					style={{
 						marginBottom: 8,
-						display: 'block'
+						display: 'block',
 					}}
 				/>
 				<Space>
 					<Button
-						type="primary"
+						type='primary'
 						onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
 						icon={<SearchOutlined />}
-						size="small"
+						size='small'
 						style={{
-							width: 90
+							width: 90,
 						}}
 					>
 						Search
 					</Button>
 					<Button
 						onClick={() => clearFilters && handleReset(clearFilters)}
-						size="small"
+						size='small'
 						style={{
-							width: 90
+							width: 90,
 						}}
 					>
 						Reset
 					</Button>
 					<Button
-						type="link"
-						size="small"
+						type='link'
+						size='small'
 						onClick={() => {
 							confirm({
-								closeDropdown: false
+								closeDropdown: false,
 							});
 							setSearchText(selectedKeys[0]);
 							setSearchedColumn(dataIndex);
@@ -73,8 +75,8 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 						Filter
 					</Button>
 					<Button
-						type="link"
-						size="small"
+						type='link'
+						size='small'
 						onClick={() => {
 							close();
 						}}
@@ -87,12 +89,11 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 		filterIcon: (filtered) => (
 			<SearchOutlined
 				style={{
-					color: filtered ? '#1677ff' : undefined
+					color: filtered ? '#1677ff' : undefined,
 				}}
 			/>
 		),
-		onFilter: (value, record) =>
-			record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+		onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
 		onFilterDropdownOpenChange: (visible) => {
 			if (visible) {
 				setTimeout(() => searchInput.current?.select(), 100);
@@ -103,7 +104,7 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 				<Highlighter
 					highlightStyle={{
 						backgroundColor: '#ffc069',
-						padding: 0
+						padding: 0,
 					}}
 					searchWords={[searchText]}
 					autoEscape
@@ -111,7 +112,7 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 				/>
 			) : (
 				text
-			)
+			),
 	});
 
 	const columns = [
@@ -120,14 +121,14 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 			dataIndex: 'plmeNombre',
 			key: 'plmeNombre',
 			width: '30%',
-			...getColumnSearchProps('plmeNombre')
+			...getColumnSearchProps('plmeNombre'),
 		},
 		{
 			title: 'Programa Academico',
 			dataIndex: ['programaAcademico', 'pracNombre'],
 			key: ['programaAcademico', 'pracNombre'],
 			width: '30%',
-			...getColumnSearchProps('pracNombre')
+			...getColumnSearchProps('pracNombre'),
 		},
 		{
 			title: 'Acción',
@@ -135,22 +136,68 @@ const TableImprovementPlan = React.memo(({ datasource = [] }) => {
 			key: 'address',
 			...getColumnSearchProps('address'),
 			sorter: (a, b) => a.address.length - b.address.length,
-			sortDirections: ['descend', 'ascend']
-		}
+			sortDirections: ['descend', 'ascend'],
+			render: (_, record) => {
+				const editable = true;
+				// const editable = isEditing(record);
+				return editable ? (
+					<span>
+						<Typography.Link
+							onClick={() => setIsModalOpen(true)}
+							style={{
+								marginRight: 8,
+							}}
+						>
+							Guardar
+						</Typography.Link>
+						<Popconfirm title='Deseas cancelar?' onConfirm={() => console.log('cancelar')} okText=' Si'>
+							<a>Cancelar</a>
+						</Popconfirm>
+					</span>
+				) : (
+					<>
+						<Typography.Link
+							/*disabled={editingKey !== ''}*/ onClick={() => console.log('edit')}
+							style={{ margin: 15 }}
+						>
+							<EditOutlined /> Editar
+						</Typography.Link>
+						<Typography.Link /*disabled={editingKey !== ''}*/ onClick={() => console.log('deleteProgram')}>
+							<DeleteOutlined /> Eliminar
+						</Typography.Link>
+					</>
+				);
+			},
+		},
 	];
 
-	return <Table columns={columns}
-			  dataSource={datasource.map(item => ({
-				  ...item,
-				  key: item.plmeId
-			  }))}
-
-	/>;
+	return (
+		<>
+			<Table
+				columns={columns}
+				dataSource={datasource.map((item) => ({
+					...item,
+					key: item.plmeId,
+				}))}
+			/>
+			{isModalOpen && (
+				<Modal
+					title='Basic Modal'
+					open={isModalOpen}
+					onOk={() => setIsModalOpen(false)}
+					onCancel={() => setIsModalOpen(false)}
+				>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Modal>
+			)}
+		</>
+	);
 });
 
 TableImprovementPlan.propTypes = {
-	datasource: PropTypes.array.isRequired
+	datasource: PropTypes.array.isRequired,
 };
 
 export default TableImprovementPlan;
-
